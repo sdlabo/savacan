55723e7f3ece1cc5c0b48231ddb9eb418c293d80
saru <saru@inf.shizuoka.ac.jp>
2016-04-21 10:07:29 +0900

    minor bugfix.

--- check_lives.php
+++ check_lives.php
@@ -14,7 +14,7 @@ function savacan_ping($host)
 function savacan_connect($host, $port){
   $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
   socket_set_option($sock, SOL_SOCKET, SO_SNDTIMEO, array("sec"=>5, "usec"=>0) );
-  return socket_connect($sock, "202.228.252.200", 10022);
+  return socket_connect($sock, $host, $port);
 }
 
 function savacan_disk()
@@ -22,19 +22,19 @@ function savacan_disk()
   return disk_free_space("/") / 1000.0 / 1000.0 / 1000.0;
 }
 
-if(savacan_disk() > 40.0){
+if(savacan_disk() < 40.0){
     savacan_mail("sarulabのディスク容量が残り40GBを切っています。");
 }
 
-if(savacan_ping("133.70.169.157") === 0){
+if(savacan_ping("133.70.169.157") === 1){
   savacan_mail("aurumが落ちているようです。");
 }
 
-if(savacan_connect("202.228.252.200", 10022) === TRUE){
+if(savacan_connect("202.228.252.200", 10022) === FALSE){
   savacan_mail("battleshipが落ちているようです。");
 }
 
-if(savacan_connect("133.70.169.129", 10022) === TRUE){
+if(savacan_connect("133.70.169.129", 14168) === FALSE){
   savacan_mail("marmorが落ちているようです。");
 }
 
